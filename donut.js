56cb55c6591e7e005a7efc812ce19fce195dd2ad
dimitri <andre.d3003@gmail.com>
2025-11-26 18:52:24 +0100

    make it pretty

--- donut.js
+++ donut.js
@@ -1,12 +1,13 @@
 (function() {
   var _onload = function() {
-    var pretag = document.getElementById("d");
-    var canvastag = document.getElementById("canvasdonut");
+    var pretag = document.getElementById("donut");
 
     var tmr1 = undefined,
       tmr2 = undefined;
     var A = 1,
       B = 1;
+    W = 60;
+    H = 18;
 
     // This is copied, pasted, reformatted, and ported directly from my original
     // donut.c code
@@ -19,8 +20,8 @@
         sA = Math.sin(A),
         cB = Math.cos(B),
         sB = Math.sin(B);
-      for (var k = 0; k < 1760; k++) {
-        b[k] = k % 80 == 79 ? "\n" : " ";
+      for (var k = 0; k < W * H; k++) {
+        b[k] = k % W == W - 1 ? "\n" : " ";
         z[k] = 0;
       }
       for (var j = 0; j < 6.28; j += 0.07) {
@@ -35,17 +36,17 @@
             D = 1 / (sp * h * sA + st * cA + 5), // this is 1/z
             t = sp * h * cA - st * sA; // this is a clever factoring of some of the terms in x' and y'
 
-          var x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
-            y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
-            o = x + 80 * y,
-            N =
-              0 |
-              (8 *
-                ((st * sA - sp * ct * cA) * cB -
-                  sp * ct * sA -
-                  st * cA -
-                  cp * ct * sB));
-          if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
+          var x = 0 | (W / 2 + W * 0.35 * D * (cp * h * cB - t * sB)),
+            y = 0 | (H / 2 + H * 0.6 * D * (cp * h * sB + t * cB)),
+            o = x + W * y;
+          N =
+            0 |
+            (8 *
+              ((st * sA - sp * ct * cA) * cB -
+                sp * ct * sA -
+                st * cA -
+                cp * ct * sB));
+          if (y < H && y >= 0 && x >= 0 && x < W - 1 && D > z[o]) {
             z[o] = D;
             b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
           }
